e22a211472e1437a91a44f8006aab2f8692f8257
AbdElsalam <abdelsalammohamed@stud.fci-cu.edu.eg>
2018-11-28 18:56:03 +0200

    Add shift array function

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    }
+
+    public ArrayList<String> shift_array (ArrayList<String> arr) {
+        ArrayList<String> shifted_arr = new ArrayList<>();
+        shifted_arr.add(arr.get(arr.size()-1));
+        for (int i = 0; i < arr.size() - 1; i ++)
+            shifted_arr.add(arr.get(i));
+        return shifted_arr;
     }
 }
